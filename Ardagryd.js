8ca0f1d47cb9bb0191dc0a8584a932a10d3c192d
Fabian Kretzer <fabian.kretzer@eddyson.de>
2016-11-28 14:25:29 +0100

    export Filter component

--- Ardagryd.js
+++ Ardagryd.js
@@ -4,6 +4,7 @@ import Column from './lib/Column';
 import Pager from './lib/Pager';
 import GridRow from './lib/GridRow';
 import GridCell from './lib/GridCell';
+import Filter from './lib/Filter';
 
 export default Ardagryd;
-export { Grid, Column, Pager, GridRow, GridCell };
+export { Grid, Column, Pager, GridRow, GridCell, Filter };
