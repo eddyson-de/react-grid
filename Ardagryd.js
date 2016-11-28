c8eb32546ac9a36d2b613a180b6b952116008e93
Fabian Kretzer <fabian.kretzer@eddyson.de>
2016-11-28 09:53:36 +0100

    export config components

--- Ardagryd.js
+++ Ardagryd.js
@@ -1,5 +1,9 @@
 import Grid from './lib/Grid';
 import Ardagryd from './lib/Ardagryd';
+import Column from './lib/Column';
+import Pager from './lib/Pager';
+import GridRow from './lib/GridRow';
+import GridCell from './lib/GridCell';
 
 export default Ardagryd;
-export { Grid };
+export { Grid, Column, Pager, GridRow, GridCell };
