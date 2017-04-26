2921c0266049b959afd07f233057cf212dbbbdb8
jochenberger <fooberger@gmail.com>
2017-04-26 09:32:38 +0200

    Adapt for jsdom API changes

--- test/setup.js
+++ test/setup.js
@@ -1,5 +1,7 @@
-var jsdom = require('jsdom');
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
 
-global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
-global.window = document.defaultView;
+const dom = new JSDOM('<!doctype html><html><body></body></html>');
+global.window = dom.window;
+global.document = window.document;
 global.navigator = {userAgent: 'node.js'};
