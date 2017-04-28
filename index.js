f1aad132eabf413e7a1d7b358cfbad8d736ffa74
Jochen Berger <fooberger@gmail.com>
2017-04-28 09:29:03 +0200

    fix import

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import ConfigCollector from './lib/ConfigCollector';
 import DefaultGridTemplate from './lib/DefaultGridTemplate'
-import { buildGridWithTemplate } from './lib/DefaultGridTemplate'
+import { buildGridWithTemplate } from './lib/GridBuilder'
 
 const DefaultGrid = buildGridWithTemplate(DefaultGridTemplate)
 
