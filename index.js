e7d6bdb8a657eaa4841a0997ee7338f813a109c5
Jochen Berger <fooberger@gmail.com>
2017-04-28 09:40:04 +0200

    fix imports

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ import ConfigCollector from './lib/ConfigCollector';
 import DefaultGridTemplate from './lib/DefaultGridTemplate'
 import { buildGridWithTemplate } from './lib/GridBuilder'
 
-import Body from "./libBody";
+import Body from "./lib/Body";
 import Row from './lib/Row'
 import Cell from './lib/Cell'
-import HeaderRow from "./libHeaderRow";
+import HeaderRow from "./lib/HeaderRow";
 import FilterRow from "./lib/FilterRow";
 import Column from './lib/Column'
-import Pager from "./Pager";
+import Pager from "./lib/Pager";
 
 
 const DefaultGrid = buildGridWithTemplate(DefaultGridTemplate)
