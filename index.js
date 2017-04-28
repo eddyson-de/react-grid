32c4172c8cc87f4872e7b57069519ae9f239cee6
Jochen Berger <fooberger@gmail.com>
2017-04-28 09:37:46 +0200

    export more stuff

--- index.js
+++ index.js
@@ -2,10 +2,26 @@ import ConfigCollector from './lib/ConfigCollector';
 import DefaultGridTemplate from './lib/DefaultGridTemplate'
 import { buildGridWithTemplate } from './lib/GridBuilder'
 
+import Body from "./libBody";
+import Row from './lib/Row'
+import Cell from './lib/Cell'
+import HeaderRow from "./libHeaderRow";
+import FilterRow from "./lib/FilterRow";
+import Column from './lib/Column'
+import Pager from "./Pager";
+
+
 const DefaultGrid = buildGridWithTemplate(DefaultGridTemplate)
 
 export {
   ConfigCollector,
   buildGridWithTemplate,
-  DefaultGrid
+  DefaultGrid,
+  Body,
+  Row,
+  Cell,
+  HeaderRow,
+  FilterRow,
+  Column,
+  Pager
 }
