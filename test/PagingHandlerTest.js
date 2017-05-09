d26a4826b4b3c66b58aa1c1c49ddadbee1198149
Fabian Kretzer <fabian.kretzer@eddyson.de>
2017-05-09 11:48:48 +0200

    PagingHandler passed context value `numberOfPages` should not be 0

--- test/PagingHandlerTest.js
+++ test/PagingHandlerTest.js
@@ -1,4 +1,5 @@
 import React          from 'react';
+import PropTypes from 'prop-types';
 import chai, { expect } from 'chai';
 import { mount } from 'enzyme';
 import chaiEnzyme from 'chai-enzyme';
@@ -6,14 +7,36 @@ chai.use(chaiEnzyme());
 
 import withPagingHandler from '../lib/PagingHandler';
 
+class MockPager extends React.Component {
+    constructor(props){
+        super(props);
+    }
+    render(){
+      const {numberOfPages} = this.context;
+      return(<span>{numberOfPages}</span>);
+    }
+}
+MockPager.contextTypes = {
+    numberOfPages: PropTypes.number
+};
+
 
 describe('Tests for default column config handling', function(){
     it('Passed down `currentPage` prop should never be zero', ()=>{
         const data = [{foo: 'bar'},{foo: 'bazinga'}];
         const ComponentWithHandler = withPagingHandler(()=><div />);
-        const Mounted = mount(<ComponentWithHandler objects={data}/>)
+        const Mounted = mount(<ComponentWithHandler objects={data}/>);
         expect(Mounted).to.have.state('currentPage', 1);
         Mounted.setProps({objects: []});
         expect(Mounted).to.have.state('currentPage', 1);
+    });
+    
+    it('numberOfPages should not be 0', ()=>{
+        const data = [{foo: 'bar'},{foo: 'bazinga'}];
+        const ComponentWithHandler = withPagingHandler(MockPager);
+        const Mounted = mount(<ComponentWithHandler objects={data}/>);
+        expect(Mounted.text()).to.equal("1");
+        Mounted.setProps({objects: []});
+        expect(Mounted.text()).to.equal("1");
     });
 });
