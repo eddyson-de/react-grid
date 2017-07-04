4e033b2b38417c39db2af3fc6557f03c5904e45e
Fabian Kretzer <fabian.kretzer@eddyson.de>
2017-07-04 10:17:39 +0200

    upgrade deps, use use upstream react-prism instead of forked version

--- documentation/CodeExample.js
+++ documentation/CodeExample.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import PrismCode from './PrismCode';
+import PrismCode from 'react-prism';
 
 const CodeExample = ({children}) => 
     <PrismCode component={'pre'} className="language-jsx">{children}</PrismCode>;
