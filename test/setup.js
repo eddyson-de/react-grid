7fd050ecc20d4f75444a5f699ddcdfa73626df66
Jochen Berger <fooberger@gmail.com>
2017-11-24 11:44:20 +0100

    upgrade to React 16

--- test/setup.js
+++ test/setup.js
@@ -21,6 +21,6 @@ global.navigator = {
 copyProps(window, global);
 
 var enzyme = require('enzyme');
-var Adapter = require('enzyme-adapter-react-15');
+var Adapter = require('enzyme-adapter-react-16');
 
 enzyme.configure({ adapter: new Adapter() });
